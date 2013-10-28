d4e60b5c775aa4aaa40da49c5debfa762900505b
Don Pavlik <git@donpavlik.net>
2013-10-28 16:28:23 -0400

    Initial commit of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeTrek::Application.config.secret_key_base = 'abf68af38ba54f17e9cb2636e36cd339bdc5cab35104e116655a4a5827b393458e7ba79f5fac0e9f5fb179a78c9208e1b3d3efe8f40c9373677c48821823de19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeTrek::Application.config.session_store :cookie_store, key: '_TimeTrek_session'
