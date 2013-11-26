1bfc5e9fa125115c99a45792ba221990be00769d
Don Pavlik <git@donpavlik.net>
2013-11-26 12:17:06 -0500

    database updates to migration scripts

--- db/migrate/20131028211338_create_tasks.rb
+++ db/migrate/20131028211338_create_tasks.rb
@@ -1,10 +1,12 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :description
-      t.string :status, default: 0
+      t.string :description, null: false
+      t.string :status, default: TaskStatus.open, null: false
 
       t.timestamps
     end
   end
 end
+
+
--- db/migrate/20131114215225_add_column_name_to_task.rb
+++ db/migrate/20131114215225_add_column_name_to_task.rb
@@ -1,5 +1,5 @@
 class AddColumnNameToTask < ActiveRecord::Migration
   def change
-    add_column :tasks, :flagged, :boolean, default: false
+    add_column :tasks, :flagged, :boolean, default: false, null: false
   end
 end
